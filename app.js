7c3d156791c2b043148a9fa5fbd9d348923d60ed
Faustino <fausticc2002@gmail.com>
2021-07-22 17:53:31 -0300

    detalle producto

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const publicPath = path.resolve(__dirname, "./public")
 app.use( express.static(publicPath) )
 
 app.listen(3030, () =>{
-    console.log("servidor corriendo en el puerto 3000")
+    console.log("servidor corriendo en el puerto 3030")
 })
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+app.get("/detalle-producto", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/detalle-producto.html"))
 })
