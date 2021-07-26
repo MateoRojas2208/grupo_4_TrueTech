544f1e25e89572fe4a732d5d747a656989c14146
Mateo_Rojas <mateorojas2208@gmail.com>
2021-07-26 20:22:47 -0300

    header

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express()
 const publicPath = path.resolve(__dirname, "./public")
 app.use( express.static(publicPath) )
 
-app.listen(3030, () =>{
-    console.log("servidor corriendo en el puerto 3030")
+app.listen(3000, () =>{
+    console.log("servidor corriendo en el puerto 3000")
 })
 
 // Paginas
