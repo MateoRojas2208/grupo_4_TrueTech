1768d187ce008ddc1640cd5bca12db670ef65d86
Mateo_Rojas <mateorojas2208@gmail.com>
2021-08-09 19:45:09 -0300

    EJS

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Express' });
 });
-
+router.get('/carrito', function(req, res, next) {
+  res.render('carrito', { title: 'Express' });
+});
+router.get('/detalle-producto', function(req, res, next) {
+  res.render('detalle-producto', { title: 'Express' });
+});
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Express' });
+});
 module.exports = router;
