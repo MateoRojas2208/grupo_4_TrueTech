317e473a6d12408999280f3c5a4e812d36c59a8d
Mateo_Rojas <mateorojas2208@gmail.com>
2021-08-23 19:31:39 -0300

    controllers

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var carritoRouter = require('./routes/carrito');
-var detailRouter = require("./routes/productDetail")
-var creationRouter = require("./routes/productCreation")
+var productRouter = require("./routes/product")
 
 var app = express();
 
@@ -22,11 +22,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/carrito', carritoRouter);
-app.use("/productDetail", detailRouter)
-app.use("/productCreation", creationRouter)
+app.use("/product", productRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
