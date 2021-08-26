db9fddf93897c69228286b7b6a9e3e2afe68a9d7
Mateo_Rojas <mateorojas2208@gmail.com>
2021-08-26 16:51:33 -0300

    jSON y rutas

--- routes/product.js
+++ routes/product.js
@@ -6,24 +6,24 @@ const router = express.Router();
 // ************ Controller Require ************
 const productsController = require('../controllers/productController');
 
-/*** GET ALL PRODUCTS ***/ 
-router.get('/', productsController.index); 
+/*** GET ALL PRODUCTS ***/
+router.get('/', productsController.index);
 
 // /*** CREATE ONE PRODUCT ***/ 
- router.get('/creation', productsController.create); 
-//  router.???('/', productsController.store); 
+router.get('/create', productsController.create);
+router.post('/', productsController.store);
 
 
 // /*** GET ONE PRODUCT ***/ 
-router.get('/detail', productsController.detail); 
+router.get('/products/:id', productsController.detail);
 
 // /*** EDIT ONE PRODUCT ***/ 
-// router.put('/:id/???', productsController.edit); 
-// router.???('/:id', productsController.update); 
+router.get('/products/:id/edit', productsController.edit);
+router.put('/:id', productsController.update);
 
 
 // /*** DELETE ONE PRODUCT***/ 
-router.delete('/:id', productsController.destroy); 
+router.delete('/:id', productsController.destroy);
 
 
 module.exports = router;
