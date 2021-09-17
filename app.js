9c5b3a3c9f20f511338c1da128674ef3b1ce842f
Mateo_Rojas <mateorojas2208@gmail.com>
2021-09-17 14:07:49 -0300

    profile page hecha

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-
+const session = require("express-session")
+const cookieParser = require("cookie-parser")
 
 var indexRouter = require('./src/routes/index');
 var usersRouter = require('./src/routes/users');
@@ -21,7 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session( {secret: "nuestro mensaje secreto" } ))
+app.use(cookieParser)
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
