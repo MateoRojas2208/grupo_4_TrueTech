a87cd7e449569fea0e171a992babf998491969e7
Mateo_Rojas <mateorojas2208@gmail.com>
2021-10-23 18:03:40 -0300

    sequelize y mysql2 config

---
+++ database/config/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "Monito123!",
+    "database": "database_truetechv2",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  }
+,
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
