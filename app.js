c442846150bdc313515dc6e8184725693127b8f5
FaustinoCorrea <86019666+FaustinoCorrea@users.noreply.github.com>
2021-11-04 16:49:46 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const session = require("express-session")
+const Sequelize = require('sequelize');
+const mysql = require("mysql2")
+
+var indexRouter = require('./src/routes/index');
+var usersRouter = require('./src/routes/users');
+var productRouter = require("./src/routes/product")
+
+var app = express();
+
+
+// view engine setup
+app.set('views', path.join(__dirname, '/src/views'));
+app.set('view engine', 'ejs');
+
+
+
+// creating 24 hours from milliseconds
+const oneDay = 1000 * 60 * 60 * 24;
+//session middleware
+app.use(session({
+  secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+  saveUninitialized: false,
+  cookie: { maxAge: oneDay },
+  resave: true
+
+}))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use("/product", productRouter)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  let errorLogin = false
+  res.render('error', { errorLogin });
+});
+
+module.exports = app;
