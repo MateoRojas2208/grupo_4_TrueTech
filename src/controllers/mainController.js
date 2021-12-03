2eeb29a728af1ab7cd17f086ef0bd1d025210afd
Faustino <fausticc2002@gmail.com>
2021-12-03 16:45:39 -0300

    Por fin anda PC creation

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-<<<<<<< HEAD
 const path = require('path'); 
 const db = require('../../database/models');
 const Product = db.Product;
@@ -11,18 +10,6 @@ const controller = {
 			res.render("home", {product: a})
 		})
 		
-=======
-const path = require('path');
-const db = require('../../database/models');
-const Product = db.Product;
-let main;
-const controller = {
-    home: (req, res) => {
-		Product.findAll().then(a => {
-            let logeado = req.session.isLogged
-            res.render("home", {product: a})
-        })
->>>>>>> e52a35ced049539c696cdfb203be74684856a550
 	},
 }
 module.exports = controller;
